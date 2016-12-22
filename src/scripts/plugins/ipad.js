e027d06d366f6a995faba43e165e8412da22bda8
Vlad Filip <vlad.filip@williamhill.com>
2016-12-22 13:04:54 +0100

    v2 merge

---
+++ src/scripts/plugins/ipad.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import { screen } from './../utils/screen.js'
+import { Blocked } from './../utils/blocked.js'
+
+export function ipad ( gf ) {
+  let styles = require( './../../styles/plugins/ipad.scss' )
+  styles.use()
+  screen.lock( true )
+
+  let blocked = new Blocked( gf.frame )
+  blocked.content( gf.config.get( 'ipad', 'blockedElement' ) )
+
+  // Show / Hide fullscreen overlay
+  let listener = screen.onChange( ( e, orientation ) => {
+    let ucOrientation = orientation.charAt( 0 ).toUpperCase() + orientation.slice( 1 )
+    // Check if current orientation is blocked
+    if ( gf.config.get( 'ipad', 'block' + ucOrientation ) ) {
+      blocked.show()
+    } else {
+      blocked.hide()
+    }
+  } )
+
+  this.destroy = () => {
+    listener()
+    screen.lock( false )
+    blocked.destroy()
+    styles.unuse()
+  }
+}
