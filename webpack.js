15bdf7b119dd6f48e017f29b02ad1877d2314f1f
Vlad Filip <vlad.filip@williamhill.com>
2017-01-23 17:22:36 +0100

    added more data to post install msg

--- webpack.js
+++ webpack.js
@@ -6,7 +6,13 @@ const config = require( __dirname + '/webpack.config.js' )
 let compiler = webpack( config )
 
 compiler.run( function ( err, stats ) {
-  if ( stats.hasErrors() ) {
+  if ( err ) {
     console.error( err )
+  }
+  if ( stats.hasErrors() ) {
+    console.warn( stats.toString( {
+      chunks: false, // Makes the build much quieter
+      colors: true
+    } ) )
   }
 } )
